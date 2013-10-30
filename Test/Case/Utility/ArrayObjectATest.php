8476acc04f3e9f3f53e1c59f123c5a380f50c25f
imsamurai <imsamurai@10.0.0.118>
2013-10-30 18:01:08 +0200

    add code and tests

---
+++ Test/Case/Utility/ArrayObjectATest.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * Author: imsamurai <im.samuray@gmail.com>
+ * Date: Oct 30, 2013
+ * Time: 5:30:59 PM
+ * Format: http://book.cakephp.org/2.0/en/development/testing.html
+ */
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'Utility' . DIRECTORY_SEPARATOR . 'ArrayObjectA.php';
+
+/**
+ * Advanced Array Object Test
+ * 
+ * @package ArrayObjectATest
+ * @subpackage Utility
+ */
+class ArrayObjectATest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setUp() {
+		parent::setUp();
+	}
+
+	/**
+	 * Test merge
+	 * 
+	 * @param ArrayObjectA $Array1
+	 * @param ArrayObjectA $Array2
+	 * @param ArrayObjectA $Result
+	 * @param bool $same
+	 * 
+	 * @dataProvider mergeProvider
+	 */
+	public function testMerge($Array1, $Array2, $Result, $same) {
+		if ($same) {
+			$this->assertEquals($Array1->merge($Array2), $Result);
+		} else {
+			$this->assertNotEquals($Array1->merge($Array2), $Result);
+		}
+	}
+
+	/**
+	 * Test map
+	 * 
+	 * @param ArrayObjectA $Array1
+	 * @param callable $callback
+	 * @param ArrayObjectA $Result
+	 * @param bool $same
+	 * 
+	 * @dataProvider mapProvider
+	 */
+	public function testMap($Array1, $callback, $Result, $same) {
+		if ($same) {
+			$this->assertEquals($Array1->map($callback), $Result);
+		} else {
+			$this->assertNotEquals($Array1->map($callback), $Result);
+		}
+	}
+
+	/**
+	 * Test filter
+	 * 
+	 * @param ArrayObjectA $Array1
+	 * @param callable $callback
+	 * @param ArrayObjectA $Result
+	 * @param bool $same
+	 * 
+	 * @dataProvider filterProvider
+	 */
+	public function testFilter($Array1, $callback, $Result, $same) {
+		if ($same) {
+			$this->assertEquals($Array1->filter($callback), $Result);
+		} else {
+			$this->assertNotEquals($Array1->filter($callback), $Result);
+		}
+	}
+
+	/**
+	 * Test reduce
+	 * 
+	 * @param ArrayObjectA $Array1
+	 * @param callable $callback
+	 * @param mixed $result
+	 * @param bool $same
+	 * 
+	 * @dataProvider reduceProvider
+	 */
+	public function testReduce($Array1, $callback, $result, $same) {
+		if ($same) {
+			$this->assertEquals($Array1->reduce($callback), $result);
+		} else {
+			$this->assertNotEquals($Array1->reduce($callback), $result);
+		}
+	}
+
+	public function mergeProvider() {
+		return array(
+			array(new ArrayObjectA(array('a', 'c', 'd')), new ArrayObjectA(array('b', 'c', 'e')), new ArrayObjectA(array('a', 'c', 'd', 'b', 'c', 'e')), true),
+			array(new ArrayObjectA(array('a', 'c', 'd')), new ArrayObjectA(array('b', 'c', 'e')), new ArrayObjectA(array('a', 'c', 'd', 'b')), false)
+		);
+	}
+
+	public function mapProvider() {
+		return array(
+			array(new ArrayObjectA(array('a', 'c', 'd')), function($e) {
+			return "$e 1";
+		}, new ArrayObjectA(array('a 1', 'c 1', 'd 1')), true),
+			array(new ArrayObjectA(array('a', 'c', 'd')), function($e) {
+			return "$e 1";
+		}, new ArrayObjectA(array('a 2', 'c 2', 'd 2')), false)
+		);
+	}
+
+	public function filterProvider() {
+		return array(
+			array(new ArrayObjectA(array('a', 'c', 'd')), function($e) {
+			return $e == 'c';
+		}, new ArrayObjectA(array('c')), true),
+			array(new ArrayObjectA(array('a', 'c', 'd')), function($e) {
+			return $e == 'd';
+		}, new ArrayObjectA(array('a', 'c', 'd')), false)
+		);
+	}
+
+	public function reduceProvider() {
+		return array(
+			array(new ArrayObjectA(array(1, 2, 3)), function($e, $acc) {
+			return (int) $acc + $e;
+		}, 1 + 2 + 3, true),
+			array(new ArrayObjectA(array('a', 'c', 'd')), function($e, $acc) {
+			return (int) $acc + 1;
+		}, 0, false)
+		);
+	}
+
+}
---
+++ Utility/ArrayObjectA.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Author: imsamurai <im.samuray@gmail.com>
+ * Date: Oct 30, 2013
+ * Time: 5:21:33 PM
+ */
+
+/**
+ * Advanced array object
+ *  
+ * @package ArrayObjectA
+ * @subpackage Utility
+ */
+class ArrayObjectA extends ArrayObject {
+
+	/**
+	 * Merge arrays
+	 * 
+	 * @param ArrayObjectA $Array
+	 * @return \ArrayObjectA
+	 */
+	public function merge(ArrayObjectA $Array) {
+		return new ArrayObjectA(array_merge($this->getArrayCopy(), $Array->getArrayCopy()));
+	}
+
+	/**
+	 * Apply callback to each element of array
+	 * 
+	 * @param callable $callback
+	 * @return \ArrayObjectA
+	 */
+	public function map(callable $callback) {
+		foreach ($this->getIterator() as $index => $ActiveRecord) {
+			$this->offsetSet($index, $callback($ActiveRecord));
+		}
+		return $this;
+	}
+
+	/**
+	 * Remove all elements that not satisfy $callback
+	 * 
+	 * @param callable $callback
+	 * @param bool $resetKeys
+	 * @return \ArrayObjectA
+	 */
+	public function filter(callable $callback, $resetKeys = true) {
+		$Iterator = new CallbackFilterIterator($this->getIterator(), $callback);
+		return new ArrayObjectA(iterator_to_array($Iterator, !$resetKeys));
+	}
+
+	/**
+	 * Reduces array into single value using $callback
+	 * 
+	 * @param callable $callback
+	 * @param mixed $init Initial value
+	 * @return mixed
+	 */
+	public function reduce(callable $callback, $init = null) {
+		return array_reduce(iterator_to_array($this->getIterator()), $callback, $init);
+	}
+
+}
